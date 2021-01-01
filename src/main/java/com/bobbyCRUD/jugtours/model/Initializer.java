699b4ee745aaf9e435cb8c65611946ddbd2ebf74
Bobby Washington <bobby.washington@coxautoinc.com>
2021-01-01 17:41:19 -0500

    Removed un-needed files.  Intialize class is working now.

--- src/main/java/com/bobbyCRUD/jugtours/model/Initializer.java
+++ src/main/java/com/bobbyCRUD/jugtours/model/Initializer.java
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Component;
 
 import java.time.Instant;
 import java.util.Collections;
-import java.util.List;
-import java.util.Set;
 import java.util.stream.Stream;
 
 @Component
